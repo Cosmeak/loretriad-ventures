70aefe47315b81db730b336cee56eca670048be8
yannisobert <90550556+yannisobert@users.noreply.github.com>
2024-03-28 15:47:56 +0100

    loretriad-ventures: Add all

--- src/routes/adventure/+page.svelte
+++ src/routes/adventure/+page.svelte
@@ -81,7 +81,7 @@
             />
             <button
                 type="submit"
-                class="w-1/6 rounded-lg bg-blue-500 py-2 px-4 text-white hover:cursor-pointer hover:bg-blue-600 focus:outline-none"
+                class="w-2/6 rounded-lg bg-blue-500 py-2 px-4 text-white hover:cursor-pointer hover:bg-blue-600 focus:outline-none"
                 >Envoyer</button
             >
         </form>
